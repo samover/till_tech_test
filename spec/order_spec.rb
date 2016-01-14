e50bd935e2a58165d0987f5b71c123b89c154f36
samover <samuel.overloop@gmail.com>
2016-01-14 11:25:45 +0000

    removed hard coded shop data

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,3 +1,10 @@
 describe Order do
 
+  let(:order) { described_class.new(shop: hipster_coffee) }
+
+  describe "#initialize" do
+    it "has the name of the shop" do
+      expect(order.shop_name).to eq "The Coffee Connection"
+    end
+  end
 end
